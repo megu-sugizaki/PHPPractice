3816298d40234954a285a872cead30494cfc76df
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-22 08:14:53 +0900

    lesson7

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,12 +1,21 @@
 <?php 
-
 $name = "Meg";
 
-// echo "It's Sunday. Hello" . $name . PHP_EOL;
-echo "It's Sunday. Hello $name" . PHP_EOL;
+//この中に書いたテキストの字下げや開業は保持。
+//始めのEOTの後は改行。EOTは好きな名前でいいが End of TextでEOTがわかりやすい
+$text = <<< 'EOT'
+hello!
+  this is looooong
+text!
+EOT;
 
-//バックスラッシュはoption + ¥
-echo "It's \"Sunday\". Hello $name" . PHP_EOL;
+echo $text;
 
-//tabを出す
-echo "It's \"Sunday\". \t Hello $name" . PHP_EOL;
+//textを""で囲むか何もつけずにおくと中に変数を埋め込める
+$text = <<< "EOT"
+hello! $name
+  this is looooong
+text!
+EOT;
+
+echo $text;
