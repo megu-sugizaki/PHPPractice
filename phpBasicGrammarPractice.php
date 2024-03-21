0ac27e25ab31f7e9a9411f116338a0366294b391
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-22 07:54:59 +0900

    lesson6-3

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -4,3 +4,9 @@ $name = "Meg";
 
 // echo "It's Sunday. Hello" . $name . PHP_EOL;
 echo "It's Sunday. Hello $name" . PHP_EOL;
+
+//バックスラッシュはoption + ¥
+echo "It's \"Sunday\". Hello $name" . PHP_EOL;
+
+//tabを出す
+echo "It's \"Sunday\". \t Hello $name" . PHP_EOL;
