7dc8b38811beb30302c9b75602cc63bbfdf4ead7
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-22 08:17:56 +0900

    lesson8

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,21 +1,6 @@
-<?php 
-$name = "Meg";
+<?php
 
-//この中に書いたテキストの字下げや開業は保持。
-//始めのEOTの後は改行。EOTは好きな名前でいいが End of TextでEOTがわかりやすい
-$text = <<< 'EOT'
-hello!
-  this is looooong
-text!
-EOT;
-
-echo $text;
-
-//textを""で囲むか何もつけずにおくと中に変数を埋め込める
-$text = <<< "EOT"
-hello! $name
-  this is looooong
-text!
-EOT;
-
-echo $text;
+echo 10 + 3 . PHP_EOL;
+echo 10 - 3 . PHP_EOL;
+echo 10 * 3 . PHP_EOL;
+echo 10 / 3 . PHP_EOL;
