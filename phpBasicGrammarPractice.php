49c82b896575c6b4678f6850db58793cbfeebf76
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-23 09:39:05 +0900

    lesson11-2

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -7,8 +7,16 @@ $d = null;
 $e = true;
 
 //データ型を調べるための命令
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// var_dump($d);
+// var_dump($e);
+
+//データ型を変えられるキャスト型という命令
+$a = (float)10;
+$b = (string)1.3;
+
 var_dump($a);
 var_dump($b);
-var_dump($c);
-var_dump($d);
-var_dump($e);
+
