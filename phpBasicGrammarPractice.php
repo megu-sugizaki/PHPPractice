17c56934bfe9cc0678e075fc79575f0f105b371a
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-23 09:36:15 +0900

    lesson11

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,17 +1,14 @@
 <?php
-// $name = 'Meg';
-// //再代入
-// $name = 'dotinstall';
 
-// // 定数, 変数名は基本大文字、ドルマークは使わない
-// define('NAME', 'Meg');
+$a = 'hello';
+$b = 20;
+$c = -1.3;
+$d = null;
+$e = true;
 
-// //エラーではないが、再代入すると警告が出る
-// define('dotinstall', 'Meg');
-// echo NAME . PHP_EOL; 
-
-const NAME = 'Meg';
-//再代入できない// //エラーではないが、再代入すると警告が出る
-const NAME = 'Alan';
-
-echo NAME . PHP_EOL;
+//データ型を調べるための命令
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
