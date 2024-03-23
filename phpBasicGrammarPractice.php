90891c6ba87319f7b7ec8632eefd3cf8a1e4c8b9
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-23 10:16:32 +0900

    lesson13

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,22 +1,11 @@
 <?php
 
-$a = 'hello';
-$b = 20;
-$c = -1.3;
-$d = null;
-$e = true;
+$score = 40;
 
-//データ型を調べるための命令
-// var_dump($a);
-// var_dump($b);
-// var_dump($c);
-// var_dump($d);
-// var_dump($e);
-
-//データ型を変えられるキャスト型という命令
-$a = (float)10;
-$b = (string)1.3;
-
-var_dump($a);
-var_dump($b);
-
+if($score >= 80){
+  echo 'Great!' . PHP_EOL;
+} else if($score >= 60){
+  echo 'Good!' . PHP_EOL;
+} else {
+  echo 'OK' . PHP_EOL;
+} 
