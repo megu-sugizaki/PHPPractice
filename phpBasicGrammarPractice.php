997a6e4d47597954bbb2c658eb28263f60bb0d60
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-25 07:10:39 +0900

    lesson19

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,8 +1,15 @@
 <?php
 
-$hp = 100;
+$hp = -15;
 
-while($hp > 0){
+// while($hp > 0){
+//   echo "Your HP: $hp" . PHP_EOL;
+//   $hp -= 15;
+// }
+
+//とりあえず1回は実行
+//セミコロン必要
+do {
   echo "Your HP: $hp" . PHP_EOL;
   $hp -= 15;
-}
+} while($hp > 0);
