13e3432508eb8e930a3d2a62dc765f5f10acf499
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-25 07:07:10 +0900

    lesson19

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,7 +1,8 @@
 <?php
 
-for ($i = 1; $i <= 5; $i++){
-// echo 'Hello' . PHP_EOL;
-echo "$i - Hello" . PHP_EOL;
-  //セミコロン不要
+$hp = 100;
+
+while($hp > 0){
+  echo "Your HP: $hp" . PHP_EOL;
+  $hp -= 15;
 }
