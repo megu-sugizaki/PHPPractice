cc2e81d87340ba072fe0e3130a0227021cb16010
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-26 07:33:26 +0900

    lesson22

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,19 +1,20 @@
 <?php
 
-function showAdd(){
+function showAdd($message = 'Ad'){
   echo'---------' . PHP_EOL;
-  echo '---Ad---' . PHP_EOL;
+  echo '---' . $message . '---' . PHP_EOL;
   echo '--------' . PHP_EOL;
 }
 
 
-showAdd();
+showAdd('Header Ad');
 echo 'Tom is great!' . PHP_EOL;
 echo 'Bob is great!' . PHP_EOL;
 
+// showAdd('Ad');
 showAdd();
 
 echo 'Steve is great!' . PHP_EOL;
 echo 'Bob is great!' . PHP_EOL;
 
-showAdd();
+showAdd('Footer Ad');
