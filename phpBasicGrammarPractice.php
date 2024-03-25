e26e3c3773f968dbe93ec431b9b01f31f2916e83
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-26 08:39:04 +0900

    lesson24-2

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,11 +1,14 @@
 <?php
 
+//関数の外すべてで有効なグローバルスコープ
 $rate = 1.1;
 
 function sum($a, $b, $c)
 {
   //rateが関数の外で定義されている場合
-  global $rate;
+  // global $rate;
+  //関数内のみで有効なローカルスコープ
+  $rate = 1.08;
   return ($a+$b+$c) * $rate;
 }
 
