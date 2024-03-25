81407598b10ee2bd86440bc2cb2a19392ea4c0da
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-26 08:10:34 +0900

    lesson23

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -2,8 +2,11 @@
 
 function sum($a, $b, $c)
 {
-  echo $a + $b + $c . PHP_EOL;
+  return $a + $b + $c . PHP_EOL;
+  echo 'Here!' . PHP_EOL;
 }
 
-sum(100, 200, 300);
-sum(300, 400, 500);
+// sum(100, 200, 300);
+// sum(300, 400, 500);
+
+echo sum(100, 200, 300) + sum(300, 400, 500) . PHP_EOL;
