a8bb49e604757f8b7a2888b6c0f8f424d14c3c8e
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-27 08:55:34 +0900

    lesson28

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -3,14 +3,7 @@
 //数字が文字列に入っていた場合はintとしてみてほしくない時強い型つけ
 declare (strict_types = 1);
 
-//引数の型の指定と返り値voidの設定
-function showInfo(string $name, int $score): void
+function getAward($score): string
 {
-  echo $name . ': ' . $score . PHP_EOL;
+  return $score >= 100 ? 'Gold Medal' : null;
 }
-
-showInfo('Meg', 40);
-// showInfo('Meg', 'dotinstall');
-
-//文字列でも可能な限り正しい型に変換
-showInfo('Meg', '4');
