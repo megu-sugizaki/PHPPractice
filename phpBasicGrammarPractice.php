f6f9396df25e41a2f53606c329c77f485a5a4c86
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-28 08:04:17 +0900

    lesson30-1

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -5,8 +5,16 @@
 // $score = 100;
 
 //最後にカンマを入れると入れ替え、削除、編集がしやすい
-$scores = [90, 60, 100,];
+//キーの設定
+$scores = [
+  'first'  => 90, 
+  'second' => 60, 
+  'third'  => 100,];
 
 //配列の中身を表示する
+//型もわかる
 var_dump($scores);
+//改行されてみやすい
 print_r($scores);
+
+echo $scores['third'] . PHP_EOL;
