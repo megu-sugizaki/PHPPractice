45c18f15468c150ec38bb539d3dc7cb0ad062e75
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-28 07:59:31 +0900

    lesson29

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,13 +1,11 @@
 <?php
 
-//数字が文字列に入っていた場合はintとしてみてほしくない時強い型つけ
-declare (strict_types = 1);
+// $score = 90;
+// $score = 40;
+// $score = 100;
 
-// 型の前に？マークでnullかその型か
-function getAward(?int $score): ?string
-{
-  return $score >= 100 ? 'Gold Medal' : null;
-}
+//最後にカンマを入れると入れ替え、削除、編集がしやすい
+$scores = [90, 60, 100,];
 
-echo getAward(150) . PHP_EOL;
-echo getAward(40) . PHP_EOL;
+//index = 添字（そえじ）
+echo $scores[1] . PHP_EOL;
