afe53d58489203525d12f341871a5eb411a35d43
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-29 08:51:16 +0900

    lesson34

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -11,3 +11,11 @@ function getStats(...$numbers){
 }
 
 print_r(getStats(1, 3, 5));
+
+//合計、平均それぞれの値を変数に代入することもできる
+list($sum, $average) = getStats(1, 3, 5);
+//上と同じ意味
+[$sum, $average] = getStats(1, 3, 5);
+
+echo $sum . PHP_EOL;
+echo $average . PHP_EOL;
