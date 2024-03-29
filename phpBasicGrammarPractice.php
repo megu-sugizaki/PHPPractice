920a0586a7a14cee38b73e93830424b4361de737
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-30 08:33:43 +0900

    lesson3

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,21 +1,14 @@
 <?php
-$name = 'Apple';
-$score = 32.246;
 
-$info = "[$name][$score]";
-echo $info . PHP_EOL;
+$input = ' dot_Meg ';
+$input = trim($input);
 
-//値を埋め込む箇所にデータ型に応じた記号語書く　文字列→s, 浮動小数点数→f
-// その後に埋め込む値を渡す
-//表示幅を指定。$nameは15桁分スペース（幅）を開ける
-//$scoreは10桁分、小数点以下は2桁分
-// $info = sprintf("[%15s][%10.2f]", $name, $score);
+// echo strlen($input) . PHP_EOL;
 
-//$nameを左詰め
-//$scoreの空いてるスペースを0で埋める
-$info = sprintf("[%-15s][%010.2f]", $name, $score);
+// //文字列の検索
+// //検索したい文字列を渡すと何文字目にあるか表示
+// echo strpos($input, '_') . PHP_EOL;
 
-echo $info . PHP_EOL;
-
-//文字列ではなく短に表示させたい
-$info = printf("[%-15s][%010.2f]" . PHP_EOL, $name, $score);
+//文字列の置換 _を-に
+$input = str_replace('_', '-', $input);
+echo $input . PHP_EOL;
