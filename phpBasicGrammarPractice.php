788bb44fcf91c130185288403809c0d670e7a601
Megumi Sugizaki <megusugizaki@gmail.com>
2024-03-31 07:55:53 +0900

    lesson6

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,13 +1,28 @@
 <?php
 
-$input = "こんにちは" ; 
-$input = trim($input);
+//文字種の異なる文でそれぞれの値を取り出したい
+//20200401Item-A 1200
+//substr(文字列, 位置, 桁数)
 
-//日本語を使う時は注意。mb_を使う
-echo mb_strlen($input) . PHP_EOL;
-echo mb_strpos($input, 'に') . PHP_EOL;
+//文字を置換したい
+//substr_replace(文字列文字列, 置換文字列, 位置, 桁数)
 
-$input = str_replace('にち', 'ばん', $input);
+$input = '20200320Item-A 1200';
 
-echo $input. PHP_EOL;
+//0文字目から8桁分
+$date = substr($input, 0, 8);
+
+//8文字目から8桁分
+$product = substr($input, 8, 8);
+
+//16文字目から4桁分 最後までの場合最後の引数は不要
+$amount = substr($input, 16);
+
+echo $date . PHP_EOL;
+echo $product . PHP_EOL;
+//echo $amount . PHP_EOL;
+//3桁以上はnumber_formatで見やすくなる
+echo number_format($amount) . PHP_EOL;
+
+
 
