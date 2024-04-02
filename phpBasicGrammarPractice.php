1d33a785764e752603a8dfd381518c9df864f6da
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-03 07:50:14 +0900

    lesson11

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,22 +1,17 @@
 <?php
-$scores = [30, 40, 50];
+$scores = [30, 40, 50, 60, 70];
 
+//配列の切り出し 配列の変数、何番目から　何個
+//元配列は変更せず新しい配列を返す
+$partial = array_slice($scores, 2, 3);
 
+//最後までの場合は最後の引数がいらない
+$partial = array_slice($scores, 2);
 
-// //先頭に要素を追加 いくつでも追加可能
-// array_unshift($scores, 10, 20);
+//マイナスをつけると末尾から何番目
+$partial = array_slice($scores, -2);
 
-// //末尾に要素を追加
-// array_push($scores, 60, 70);
-
-// //末尾に一文字だけの場合
-// $scores[] = 80;
-
-//要素を削除　1つ目の要素を削除
-// array_shift($scores);
-
-//末尾の要素を削除
-array_pop($scores);
 
 print_r($scores);
+print_r($partial);
 
