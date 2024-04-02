e6f28518164c2a1538e997a396c30ec7ec125cf5
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-02 21:53:11 +0900

    lesson9-2

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,14 +1,22 @@
 <?php
-$n = 5.6283;
+// $n = 5.6283;
 
-//小数点以下切り上げ
-echo ceil($n) . PHP_EOL;
+// //小数点以下切り上げ
+// echo ceil($n) . PHP_EOL;
 
-//小数点以下切り捨て
-echo floor($n) . PHP_EOL;
+// //小数点以下切り捨て
+// echo floor($n) . PHP_EOL;
 
-//四捨五入して整数に
-echo round($n) . PHP_EOL;
+// //四捨五入して整数に
+// echo round($n) . PHP_EOL;
 
-//小数点以下2桁になるように四捨五入
-echo round($n, 2) . PHP_EOL;
+// //小数点以下2桁になるように四捨五入
+// echo round($n, 2) . PHP_EOL;
+
+
+//乱数を作る（1以上6以下）
+echo mt_rand(1,6) . PHP_EOL;
+
+//最大値、最小値
+echo max(3, 9, 4) . PHP_EOL;
+echo min(3, 9, 4) . PHP_EOL;
