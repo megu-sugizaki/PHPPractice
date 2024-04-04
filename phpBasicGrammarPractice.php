9ac3a9d132adc0d9f4f42407f1d682b52f02b2ae
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-04 22:19:45 +0900

    lesson13

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,16 +1,5 @@
 <?php
-$scores = [30, 40, 50, 60, 70, 80];
 
-//途中の要素を削除してつめたい 配列、位置、個数
-//元配列を直接変更
-// array_splice($scores, 2, 3);
+$scores = [40, 50, 20, 30];
 
-// print_r($scores);
-
-//削除した位置に要素を挿入できる, 何番目から何個分削除してこの要素を入れる
-// array_splice($scores,2, 3, 100);
-// print_r($scores);
-
-//削除せずに間に要素を挿入 加えて複数要素を挿入
-array_splice($scores, 2, 0, [100, 101]);
-print_r($scores)!;
+print_r($scores);
