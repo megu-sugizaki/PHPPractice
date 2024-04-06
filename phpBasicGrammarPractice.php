046fce8084b0a8cec6143a12472ad6332d28159f
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-06 11:53:26 +0900

    lesson13

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -9,6 +9,14 @@ print_r($scores);
 shuffle($scores);
 print_r($scores);
 
+//ランダムに配列の中身を返す $scoresから2こ
+//新しい配列を返すので変数で受ける
+//帰ってくるのは$scoresのキーの方
+$picked = array_rand($scores, 2);
+//値をピックアップする場合はこう
+echo $scores[$picked[0]] . PHP_EOL;
+echo $scores[$picked[1]] . PHP_EOL;
+
 //元配列を直接書き換え
 
 
