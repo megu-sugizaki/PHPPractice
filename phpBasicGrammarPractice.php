8dbe316ebe49a88a18ce394b46542fb78f81f26e
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-07 12:55:19 +0900

    lesson13

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -3,14 +3,21 @@
 $scores = [40, 50, 20, 30];
 
 //小さい順に並び替え
+//配列を直接書き換え
 sort($scores);
 print_r($scores);
 
+//大きい順に並び替え
+rsort($scores);
+print_r($scores);
+
+//値をシャッフル
+//元配列を書き換え
 shuffle($scores);
 print_r($scores);
 
 //ランダムに配列の中身を返す $scoresから2こ
-//新しい配列を返すので変数で受ける
+//元配列は変えず新しい配列を返すので変数で受ける
 //帰ってくるのは$scoresのキーの方
 $picked = array_rand($scores, 2);
 //値をピックアップする場合はこう
