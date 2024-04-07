0f0c292715453e428bcc64afb6d1266bb9670877
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-07 12:57:35 +0900

    lesson14

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,31 +1,6 @@
 <?php
 
-$scores = [40, 50, 20, 30];
-
-//小さい順に並び替え
-//配列を直接書き換え
-sort($scores);
+//要素を一気に埋めて配列を作る
+//0のインデックスから5個分を10の値で埋める
+$scores = array_fill(0, 5, 10);
 print_r($scores);
-
-//大きい順に並び替え
-rsort($scores);
-print_r($scores);
-
-//値をシャッフル
-//元配列を書き換え
-shuffle($scores);
-print_r($scores);
-
-//ランダムに配列の中身を返す $scoresから2こ
-//元配列は変えず新しい配列を返すので変数で受ける
-//帰ってくるのは$scoresのキーの方
-$picked = array_rand($scores, 2);
-//値をピックアップする場合はこう
-echo $scores[$picked[0]] . PHP_EOL;
-echo $scores[$picked[1]] . PHP_EOL;
-
-//元配列を直接書き換え
-
-
-//ランダムに要素を2つピックアップ
-//
