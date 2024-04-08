0500ed1295ac650b0ddfbbcf394a44b2670a0660
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-09 06:45:01 +0900

    lesson15

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,23 +1,10 @@
 <?php
 
-//要素を一気に埋めて配列を作る
-//0のインデックスから5個分を10の値で埋める
-// $scores = array_fill(0, 5, 10);
-// print_r($scores);
+//配列同士の連結
+array_merge();
 
-//1~10までの値を使って配列を作る
-// $scores = range(1,10);
-// print_r($scores);
+//前配列からあと配列をひく
+array_diff();
 
-$scores = range(1,10,2);
-print_r($scores);
-
-//配列の要素を集計する
-echo array_sum($scores) . PHP_EOL;
-
-//最大値と最小値
-echo max($scores) . PHP_EOL;
-echo min($scores) . PHP_EOL;
-
-//平均(一旦合計を出して要素の数で割る)
-echo array_sum($scores) / count($scores) . PHP_EOL;
+//共通要素を抽出
+array_intersect();
