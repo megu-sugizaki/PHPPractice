1bc1f8d3a924146af9fdec8ca102cfc0c26f90d1
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-09 07:10:21 +0900

    lesson17

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,28 +1,14 @@
 <?php
-$a = [3, 4, 8];
-$b = [4, 8, 12];
+$prices =[100, 200, 300];
 
-//配列同士の連結
-// $merged = array_merge($a, $b);
-//上と同じ意味
-$merged = [...$a, ...$b];
+//左の関数を右の配列の要素全てに適用
+//$prices全ての要素に10%の税をかける
+$newPrices = array_map(
+  //無名関数 配列の引数は$nに代入
+  // function($n){return $n * 1.1; },
+  //上と同じ、アロー関数を使用
+  fn($n) => $n * 1.1,
+  $prices
+);
 
-// print_r($merged);
-
-//重複した値を取り除く
-//元配列に変更なし、新たに配列を作成
-$uniques = array_unique($merged);
-// print_r($uniques);
-
-//前配列からあと配列をひく($aから$bを引く)
-//新しい配列を返す
-$diff1 = array_diff($a, $b);
-print_r($diff1);
-
-//$bから$aをひく
-$diff2 = array_diff($b, $a);
-print_r($diff2);
-
-//共通要素を抽出
-$common = array_intersect($a, $b);
-print_r($common);
+print_r($newPrices);
