14a41d8982f5c4d7145182a85f7a22448653f48f
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-10 06:53:32 +0900

    lesson21

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,28 +1,27 @@
 <?php
-$scores = [
-'Meg' => 80,
-'Chihiro' => 70,
-'Taka' => 60,
+$data = [
+  ['name' => 'Meg', 'score' => 80],
+  ['name' => 'Chihiro', 'score' => 60],
+  ['name' => 'Taka', 'score' => 70],
+  ['name' => 'Henry', 'score' =>60],
 ];
 
-//sortやrsortだとキーが削除されて連番になる
-// sort($scores);
-// print_r($scores);
-// rsort($scores);
-// print_r($scores);
+//$scoreの小さい順
+//何を基準に並べ替えるか自分で定義できる
+//元配列を直接書き換える
+usort(
+  //まず配列を渡す
+  $data,
+  //2つの値が同じで順番を変えたくない時は0を返す
+  function($a, $b){
+    if($a['score'] === $b['score']){
+      return 0;
+    }
+    //$a > $ $bなら1を、そうじゃないなら-1を返す
+    return $a['score'] > $b['score'] ? 1 : -1;
+  }
+);
 
-//キーを保持したい
-//値小さい順
-asort($scores);
-print_r($scores);
-//値大きい順
-arsort($scores);
-print_r($scores);
-
-//キーでソートしたい
-//キーアルファベット小さい順
-ksort($scores);
-print_r($scores);
-//キーアルファベット大きい順
-krsort($scores);
-print_r($scores);
+//scoreの順に並び替えられている→usortでは1つの項目しか並び替えられない
+//scoreが同じ時、nameはアルファベット順にならない
+print_r($data);
