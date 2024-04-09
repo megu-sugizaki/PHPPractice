efdca31735653a482778a4f34a2177165f72b958
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-10 06:36:57 +0900

    lesson20

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -5,24 +5,24 @@ $scores = [
 'Taka' => 60,
 ];
 
-//キーだけ配列で取り出したい
-$keys = array_keys($scores);
+//sortやrsortだとキーが削除されて連番になる
+// sort($scores);
+// print_r($scores);
+// rsort($scores);
+// print_r($scores);
 
-//値だけ取り出したい
-$values = array_values($scores);
+//キーを保持したい
+//値小さい順
+asort($scores);
+print_r($scores);
+//値大きい順
+arsort($scores);
+print_r($scores);
 
-print_r($keys);
-print_r($values);
-
-//特定のキーがあるか調べたい
-if(array_key_exists('Meg', $scores) === true){
-  echo 'Meg is here' . PHP_EOL;
-}
-
-//特定の値があるか調べたい
-if(in_array(80, $scores) === true){
-  echo '80 is here!' . PHP_EOL;
-}
-
-//値を配列から検索して対応するキーをかえす
-echo array_search(70, $scores). PHP_EOL;
+//キーでソートしたい
+//キーアルファベット小さい順
+ksort($scores);
+print_r($scores);
+//キーアルファベット大きい順
+krsort($scores);
+print_r($scores);
