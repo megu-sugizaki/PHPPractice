345a1387372c2281c6871e6bb12d803988299a9e
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-12 07:25:25 +0900

    lesson32

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -13,3 +13,14 @@ echo time() . PHP_EOL;
 echo date('Y-m-d l', time()) . PHP_EOL;
 //time()は省略可能
 echo date('Y-m-d l') . PHP_EOL;
+
+//特定日時のtimeスタンプをいちいち計算したくない場合mktimeでその日時のunix time スタンプを作る
+//mktimeの引数は時、分、秒、月、日、年
+echo date('Y-m-d l', mktime(0, 0, 0, 5, 1)) . PHP_EOL;
+
+//日時っぽい文字列からunix timeスタンプを作る
+echo date('Y-m-d l', strtotime('2020-05-07')) . PHP_EOL;
+//↑から一日足したい場合
+echo date('Y-m-d l', strtotime('2020-05-07')) . PHP_EOL;
+
+//32ビットのマシンでは2038年以降うまく動作しないのでDateTimeクラスを使用する
