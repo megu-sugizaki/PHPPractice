eef286525825636ef3b09a3a1dea4709bb29e1c2
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-12 07:19:23 +0900

    lesson32

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,21 +1,15 @@
 <?php
-if(!file_exists('data/saburo.txt')){
-  echo 'saburo not here!' . PHP_EOL;
-//このファイルの存在確認でsaburoがなかったら処理を止めたい場合
-  exit;
-}
+//UNIXタイムスタンプ→基準はGMT、時間が数値で出てくる
+//date(フォーマット, UNIXタイムスタンプ)→date('Y-m-d', time());
+//dateはローカルタイムで表示
+//dateの中身
+//H:i:s → 時間:分:秒, l → 曜日, W → 1年のうち第何周目か
 
-//ディレクトリやファイルが存在するか確認
-if(file_exists('data') === true){
-  echo 'data exists!' . PHP_EOL;
-}
+//1秒ごとに実行すると1ずつ増える
+echo time() . PHP_EOL;
 
-//ファイルが書き込み可能か調べる
-if (is_writable('data/taro.txt') === true){
-  echo 'taro is writable' . PHP_EOL;
-}
-
-//ファイルが読み込み可能か調べる
-if (is_readable('data/taro.txt') === true){
-  echo 'taro is readable' . PHP_EOL;
-}
+//↑は数値なのでdate関数でわかりやすく表示
+//日付 + 時間
+echo date('Y-m-d l', time()) . PHP_EOL;
+//time()は省略可能
+echo date('Y-m-d l') . PHP_EOL;
