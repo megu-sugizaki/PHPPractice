c2404be43b4a35e5468a182a7b7f8e9fe3d592da
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-12 07:07:42 +0900

    lesson30

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,37 +1,21 @@
 <?php
-//dataディレクトリをターミナルで作成
-//mkdir data
-
-// //dataディレクトリにファイルを追加
-// file_put_contents('data/taro.txt', "taro\n");
-// file_put_contents('data/taro.txt', "jiro\n");
-
-// //実行するとファイルは見れないがデータはできる
-//ls -la data でデータファイルの中身があることを確認
-
-//dataのファイル一覧を表示したい
-//ファイルを開く
-// $dp = opendir('data');
-//内容を一行ずつ読み込み、読み込むものがなくなったらfalse
-//falseでない場合は{}の処理
-// while(($item = readdir($dp)) !== false){
-//   //$itemが.や..の場合はloopをスキップ
-//   //結果に.や..が表示されることを防ぐ
-//   if ($item === "." || $item === ".."){
-//     continue;
-//   }
-
-//   echo $item . PHP_EOL;
-// }
-
-//1行ずつ読み込む別の方法
-//globにpathをわたす(ワイルドカード*を使う)
-//配列で返してくれるのでforeachでとりだす
-foreach(glob('data/*.txt') as $item){
-  echo $item . PHP_EOL;
+if(!file_exists('data/saburo.txt')){
+  echo 'saburo not here!' . PHP_EOL;
+//このファイルの存在確認でsaburoがなかったら処理を止めたい場合
+  exit;
 }
 
-//ファイル名だけを取得したい場合
-foreach(glob('data/*.txt') as $item){
-  echo basename($item) . PHP_EOL;
+//ディレクトリやファイルが存在するか確認
+if(file_exists('data') === true){
+  echo 'data exists!' . PHP_EOL;
+}
+
+//ファイルが書き込み可能か調べる
+if (is_writable('data/taro.txt') === true){
+  echo 'taro is writable' . PHP_EOL;
+}
+
+//ファイルが読み込み可能か調べる
+if (is_readable('data/taro.txt') === true){
+  echo 'taro is readable' . PHP_EOL;
 }
