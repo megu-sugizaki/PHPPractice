d597105afcc5ebe00c976da106c60f4fcdfeb8f0
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-13 00:00:39 +0900

    Object lesson2

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,26 +1,15 @@
 <?php
-//UNIXタイムスタンプ→基準はGMT、時間が数値で出てくる
-//date(フォーマット, UNIXタイムスタンプ)→date('Y-m-d', time());
-//dateはローカルタイムで表示
-//dateの中身
-//H:i:s → 時間:分:秒, l → 曜日, W → 1年のうち第何周目か
+$posts = [];
 
-//1秒ごとに実行すると1ずつ増える
-echo time() . PHP_EOL;
+//空の配列に投稿といいね数のデータを挿入
+$posts[0] = ['text' => 'hello', 'likes' => 0];
+$posts[1] = ['text' => 'hello again!', 'likes' => 0];
+// print_r($posts);
 
-//↑は数値なのでdate関数でわかりやすく表示
-//日付 + 時間
-echo date('Y-m-d l', time()) . PHP_EOL;
-//time()は省略可能
-echo date('Y-m-d l') . PHP_EOL;
+function show ($post){
+  //textとlikesを表示
+  printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
+}
 
-//特定日時のtimeスタンプをいちいち計算したくない場合mktimeでその日時のunix time スタンプを作る
-//mktimeの引数は時、分、秒、月、日、年
-echo date('Y-m-d l', mktime(0, 0, 0, 5, 1)) . PHP_EOL;
-
-//日時っぽい文字列からunix timeスタンプを作る
-echo date('Y-m-d l', strtotime('2020-05-07')) . PHP_EOL;
-//↑から一日足したい場合
-echo date('Y-m-d l', strtotime('2020-05-07')) . PHP_EOL;
-
-//32ビットのマシンでは2038年以降うまく動作しないのでDateTimeクラスを使用する
+show($posts[0]);
+show($posts[1]);
