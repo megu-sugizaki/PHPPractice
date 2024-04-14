5aa484b6f9de5a9d2510592edb6f069b18b8edad
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-14 20:58:04 +0900

    lesson3

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,15 +1,29 @@
 <?php
-$posts = [];
-
-//空の配列に投稿といいね数のデータを挿入
-$posts[0] = ['text' => 'hello', 'likes' => 0];
-$posts[1] = ['text' => 'hello again!', 'likes' => 0];
-// print_r($posts);
-
-function show ($post){
-  //textとlikesを表示
-  printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
+<?php
+//クラス
+class Post
+{
+  //変数を定義 ＝ プロパティ（クラス内で宣言する変数）
+  public $text;
+  public $likes;
+  
+  //関数を定義 = メソッド このclassのデータ型が持つ変数を扱うので引数はなし
+  public function show()
+  {
+    //class内で変数にアクセスするには$this->を使う
+    printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
+  }
 }
 
-show($posts[0]);
-show($posts[1]);
+
+// $posts = [];
+// $posts[0] = ['text' => 'hello', 'likes' => 0];
+// $posts[1] = ['text' => 'hello again', 'likes' => 0];
+
+// function show($post)
+// {
+//   printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
+// }
+
+// show($posts[0]);
+// show($posts[1]);
