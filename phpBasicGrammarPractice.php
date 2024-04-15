6bfc13a29eaeb625ebb35e4f08026b5d00f60f8d
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-15 21:19:36 +0900

    lesson5

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -5,6 +5,18 @@ class Post
   // プロパティ
   public $text;
   public $likes;
+
+  //newした時に実行される
+  // public function __construct($textFromNew, $likesFromNew){
+  //   $this->text = $textFromNew;
+  //   $this->likes = $likesFromNew;
+  // }
+
+  //引数とプロパティを同じ値にすることも可能
+    public function __construct($text, $likes){
+    $this->text = $text;
+    $this->likes = $likes;
+  }
   
   // メソッド
   public function show()
@@ -15,13 +27,13 @@ class Post
 
 $posts = [];
 // $posts[0] = ['text' => 'hello', 'likes' => 0];
-$posts[0] = new Post(); //インスタンス
-$posts[0] ->text = 'hello';
-$posts[0]->likes = 0;
+$posts[0] = new Post('hello', 0); //インスタンス
+// $posts[0] ->text = 'hello';
+// $posts[0]->likes = 0;
 // $posts[1] = ['text' => 'hello again', 'likes' => 0];
-$posts[1] = new Post(); //インスタンス
-$posts[1] ->text = 'hello again';
-$posts[1]->likes = 0;
+$posts[1] = new Post('hello again', 0); //インスタンス
+// $posts[1] ->text = 'hello again';
+// $posts[1]->likes = 0;
 
 //クラス内で定義しているので不要
 // function show($post)
