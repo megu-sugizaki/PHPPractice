f510406fce36f657d8e3a8e2996cc0769828dea9
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-15 21:14:27 +0900

    lesson4

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,25 +1,29 @@
 <?php
-<?php
-//クラス
+// クラス
 class Post
 {
-  //変数を定義 ＝ プロパティ（クラス内で宣言する変数）
+  // プロパティ
   public $text;
   public $likes;
   
-  //関数を定義 = メソッド このclassのデータ型が持つ変数を扱うので引数はなし
+  // メソッド
   public function show()
   {
-    //class内で変数にアクセスするには$this->を使う
     printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
   }
 }
 
-
-// $posts = [];
+$posts = [];
 // $posts[0] = ['text' => 'hello', 'likes' => 0];
+$posts[0] = new Post(); //インスタンス
+$posts[0] ->text = 'hello';
+$posts[0]->likes = 0;
 // $posts[1] = ['text' => 'hello again', 'likes' => 0];
+$posts[1] = new Post(); //インスタンス
+$posts[1] ->text = 'hello again';
+$posts[1]->likes = 0;
 
+//クラス内で定義しているので不要
 // function show($post)
 // {
 //   printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
@@ -27,3 +31,6 @@ class Post
 
 // show($posts[0]);
 // show($posts[1]);
+
+$posts[0]->show();
+$posts[1]->show();
