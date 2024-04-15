dce66f289d1f16eb4fcc93c99bee6aa350ec794c
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-16 06:44:09 +0900

    lesson7

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -4,8 +4,10 @@ class Post
 {
   // プロパティ
   public $text;
-  //宣言する時にlikesを0に初期化していく
-  public $likes = 0;
+  //宣言する時にlikesを0に初期化していく 
+  //privateにしてclassの中でしか使えないようにする
+  //アクセス就職私をつけ忘れるとpublicになる
+  private $likes = 0;
 
 
   //引数とプロパティを同じ値にすることも可能
@@ -26,6 +28,9 @@ $posts = [];
 $posts[0] = new Post('hello'); //インスタンス
 
 $posts[1] = new Post('hello again'); //インスタンス
+
+//$posts[0]にいいねが1 ただし外から操作できてしまうのはセキュリティ的によくない
+// $posts[0] -> likes ++;
 
 $posts[0]->show();
 $posts[1]->show();
