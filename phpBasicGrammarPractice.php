ad5d35915aae6668d122d9cabcba3e90e2840fbb
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-16 07:32:04 +0900

    lesson11

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -10,6 +10,12 @@ class Post
   //クラスプロパティ
   private static $count = 0;
 
+  //classに紐づいた定数を定義
+  //大文字にして$マークはつけない
+  //定数はうっかり別の値になることはないのでpublicでもOK
+  //クラスに紐づいた定数 = オブジェクト定数
+  public const VERSION = 0.1;
+
     public function __construct(string $text){
     $this->text = $text;
 
@@ -28,14 +34,15 @@ class Post
   //クラスメソッド
   public static function showInfo(){
     printf('Count: %d' . PHP_EOL, self::$count);
+
+    //浮動小数で小数点以下1桁まで表示の設定
+    printf('VERSION: %.1f' . PHP_EOL, self::VERSION);
   }
 
 }
 
 $posts = [];
-
 $posts[0] = new Post( 'hello'); //インスタンス
-
 $posts[1] = new Post('hello again'); //インスタンス
 
 $posts[0]->show();
@@ -44,3 +51,6 @@ $posts[1]->show();
 //static関連のメソッドの呼び出し
 //2つインスタンスを作ったので2とひょうじされる
 Post::showInfo();
+
+//publicにしたのでクラスから直接呼び出すことができる
+echo Post::VERSION .PHP_EOL;
