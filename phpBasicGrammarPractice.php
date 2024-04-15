a778b6fc2f81a0b6d68efc209cbfeef82fa89ab6
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-16 07:00:41 +0900

    lesson8

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -3,16 +3,12 @@
 class Post
 {
   // プロパティ
-  public $text;
+  private $text;
   //宣言する時にlikesを0に初期化していく 
   //privateにしてclassの中でしか使えないようにする
   //アクセス就職私をつけ忘れるとpublicになる
   private $likes = 0;
 
-
-  //引数とプロパティを同じ値にすることも可能
-  //likesは0からスタートしたいので初期値を0
-  //引数からも$likesを削除
     public function __construct($text){
     $this->text = $text;
   }
@@ -22,6 +18,18 @@ class Post
   {
     printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
   }
+
+  //likeにclassの外からアクセスできるようにするメソッド
+  //-などの値にならないように
+  public function like(){
+    $this->likes++;
+
+    //ロジックも追加できる
+    if($this->likes > 100){
+      $this->likes = 100;
+    }
+  }
+
 }
 
 $posts = [];
@@ -31,6 +39,8 @@ $posts[1] = new Post('hello again'); //インスタンス
 
 //$posts[0]にいいねが1 ただし外から操作できてしまうのはセキュリティ的によくない
 // $posts[0] -> likes ++;
+
+$posts[0]->like();
 
 $posts[0]->show();
 $posts[1]->show();
