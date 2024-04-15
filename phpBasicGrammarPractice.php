fc45b3952edb97d647f2e3d07bd2e50e8aefc921
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-16 07:09:00 +0900

    lesson9

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,15 +1,16 @@
 <?php
+//強い型つけができる
+declare(strict_types=1);
+
 // クラスにまとめると後から修正が楽　コードの見通しもよくなる
 class Post
 {
-  // プロパティ
-  private $text;
-  //宣言する時にlikesを0に初期化していく 
-  //privateにしてclassの中でしか使えないようにする
-  //アクセス就職私をつけ忘れるとpublicになる
+  // プロパティ 型を指定できるが弱い型つけ。
+  // 数値を入れても文字列にしてくれてしまう
+  private string $text;
   private $likes = 0;
 
-    public function __construct($text){
+    public function __construct(string $text){
     $this->text = $text;
   }
   
@@ -19,28 +20,14 @@ class Post
     printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
   }
 
-  //likeにclassの外からアクセスできるようにするメソッド
-  //-などの値にならないように
-  public function like(){
-    $this->likes++;
-
-    //ロジックも追加できる
-    if($this->likes > 100){
-      $this->likes = 100;
-    }
-  }
-
 }
 
 $posts = [];
-$posts[0] = new Post('hello'); //インスタンス
+
+//strict にしたので型が合わない
+// $posts[0] = new Post(5); //インスタンス
 
 $posts[1] = new Post('hello again'); //インスタンス
-
-//$posts[0]にいいねが1 ただし外から操作できてしまうのはセキュリティ的によくない
-// $posts[0] -> likes ++;
-
-$posts[0]->like();
 
 $posts[0]->show();
 $posts[1]->show();
