9f0aa40e25dfed49656db437da492512aad38d64
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-19 08:20:26 +0900

    lesson20

--- phpBasicGrammarPractice.php
+++ phpBasicGrammarPractice.php
@@ -1,4 +1,14 @@
 <?php
+//PostクラスとPremiumPostクラスのみlikeメソッドを持たせたい
+//親クラスは2つ継承できないのでもう1つ用意するのはNG
+//抽象メソッドを親クラスに定義するとSponsoredPostまで継承しないといけないのでNG
+//→インターフェースを使うと、好きなクラスにメソッド定義を強制させられる
+//クラスはいくつでも実装可能で、継承関係にないクラスにも実装可能
+
+interface LikeInterface{
+  public function like();
+
+}
 
 abstract class BasePost
 {
@@ -12,17 +22,18 @@ abstract class BasePost
   abstract public function show();
 }
 
-//PostクラスとPremiumPostクラスのみlikeメソッドを持たせたい
-//親クラスは2つ継承できないのでもう1つ用意するのはNG
-//抽象メソッドを親クラスに定義するとSponsoredPostまで継承しないといけないのでNG
-//→インターフェースを使うと、好きなクラスにメソッド定義を強制させられる
-//クラスはいくつでも実装可能で、継承関係にないクラスにも実装可能
 
-class Post extends BasePost
+class Post extends BasePost implements LikeInterface
 {
+  private $likes = 0;
+
+  public function like(){
+    $this -> likes ++;
+  }
+
   public function show()
   {
-    printf('%s' . PHP_EOL, $this->text);
+    printf('%s (%d)' . PHP_EOL, $this->text, $this -> likes);
   }
 }
 
@@ -42,8 +53,14 @@ class SponsoredPost extends BasePost
   }
 }
 
-class PremiumPost extends BasePost
+class PremiumPost extends BasePost implements LikeInterface
 {
+  private $likes = 0;
+
+  public function like(){
+    $this -> likes ++;
+  }
+
   private $price;
 
   public function __construct($text, $price)
@@ -54,7 +71,7 @@ class PremiumPost extends BasePost
 
   public function show()
   {
-    printf('%s [%d JPY]' . PHP_EOL, $this->text, $this->price);
+    printf('%s (%d) [%d JPY]' . PHP_EOL, $this->text, $this -> likes, $this->price);
   }
 }
 
@@ -63,6 +80,9 @@ $posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
 $posts[2] = new SponsoredPost('hello hello', 'dotinstall');
 $posts[3] = new PremiumPost('hello there', 300);
+
+$posts[0] -> like();
+$posts[3] -> like();
 
 function processPost(BasePost $post) 
 {
