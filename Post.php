e59c0aa049b65c98dd9d5e591824d0bbbc44cbf9
Megumi Sugizaki <megusugizaki@gmail.com>
2024-04-20 15:10:52 +0900

    lesson24

---
+++ Post.php
@@ -0,0 +1,15 @@
+<?php
+class Post
+{
+  private $text;
+
+  function __construct($text)
+  {
+    $this->text = $text;
+  }
+
+  function show()
+  {
+    printf('%s' . PHP_EOL, $this->text);
+  }
+}
--- main.php
+++ main.php
@@ -1,19 +1,19 @@
 <?php
 
-class Post
-{
-  private $text;
+//post classをPost.phpに移動したのでPost.phpを読み込む
 
-  function __construct($text)
-  {
-    $this->text = $text;
-  }
+//ファイルが読み込めなかった時にエラーになって処理がとまる
+//Post classがないとそもそも動作しないのでrequireを使う
+require('Post.php');
+//上記と同じだが、読み込まないだけで処理が止まらない
+//htmlファイル等読み込めなくてもあまり問題がないものに使う
+// include('Post.php');
 
-  function show()
-  {
-    printf('%s' . PHP_EOL, $this->text);
-  }
-}
+
+//ファイルがすでに読み込まれていたらスキップしてくれる
+//ファイル構成が複雑になると使う
+// require_once('Post.php');
+// include_once('Post.php');
 
 $posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
